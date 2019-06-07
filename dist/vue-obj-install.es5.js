44bfb5c4ac8d316050c0f4d0a8b0aba5e2904f28
Sebastian Kaźmierczak <moques93@gmail.com>
2019-06-07 08:50:53 +0200

    Update dependency

--- dist/vue-obj-install.es5.js
+++ dist/vue-obj-install.es5.js
@@ -1,38 +1,39 @@
-'use strict';
+"use strict";
 
 Object.defineProperty(exports, "__esModule", {
-	value: true
+  value: true
 });
+exports["default"] = void 0;
+
 /* eslint-disable */
-
 var addNamespaces = function addNamespaces(Vue, namespaces, value) {
-	if (namespaces.length === 1) {
-		Vue[namespaces[0]] = value;
-		Vue.prototype[namespaces[0]] = value;
-	} else {
-		var namespace = namespaces.shift();
-		var ret = addNamespaces(!(namespace in Vue) ? {} : Vue[namespace], namespaces, value);
-		Vue[namespace] = ret;
-		Vue.prototype[namespace] = ret;
-	}
+  if (namespaces.length === 1) {
+    Vue[namespaces[0]] = value;
+    Vue.prototype[namespaces[0]] = value;
+  } else {
+    var namespace = namespaces.shift();
+    var ret = addNamespaces(!(namespace in Vue) ? {} : Vue[namespace], namespaces, value);
+    Vue[namespace] = ret;
+    Vue.prototype[namespace] = ret;
+  }
 
-	return Vue;
+  return Vue;
 };
 
 var Install = function Install(namespaces, obj) {
-	if (!Array.isArray(namespaces)) {
-		namespaces = namespaces.split('.');
-	}
+  if (!Array.isArray(namespaces)) {
+    namespaces = namespaces.split('.');
+  }
 
-	var first = namespaces[0];
-	return {
-		install: function install(Vue) {
-			var tmp = addNamespaces(Vue, namespaces, obj);
-
-			Vue[first] = tmp[first];
-			Vue.prototype[first] = tmp[first];
-		}
-	};
+  var first = namespaces[0];
+  return {
+    install: function install(Vue) {
+      var tmp = addNamespaces(Vue, namespaces, obj);
+      Vue[first] = tmp[first];
+      Vue.prototype[first] = tmp[first];
+    }
+  };
 };
 
-exports.default = Install;
+var _default = Install;
+exports["default"] = _default;
